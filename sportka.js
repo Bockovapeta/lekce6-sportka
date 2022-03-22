59fe3bcd96c57d16b2384f849b9cc21dbbc5dec1
Petra Bočková <Wickky@seznam.cz>
2022-03-22 21:14:32 +0100

    změna

--- sportka.js
+++ sportka.js
@@ -4,21 +4,46 @@ let osudi = [];
 let tazenaCisla = [];
 let vyherniCisla = document.querySelector('#vyherni-cisla');
 
-// generovani osudi
-for (let i = 1; i <= 49; i = i + 1) {
-    osudi.push(i);
+
+function losuj() {
+
+    vyherniCisla.innerHTML = "";
+    // nebo také můžeme zapsat let buttonLosuj = "";
+    // a pak ho definovat na Addeventlistener buttonLosuj.Addeventlistener (click, losuj());
+
+    // generovani osudi
+    for (let i = 1; i <= 49; i = i + 1) {
+        osudi.push(i);
+    }
+
+    // z osudi vyberu 7 nahodnych cisel
+    for (let i = 0; i < 7; i++) {
+        let vyherniIndex = Math.floor(Math.random() * osudi.length);
+        let vyherniCislo = osudi[vyherniIndex];
+
+        tazenaCisla.push(vyherniCislo);
+        osudi.splice(vyherniIndex, 1);
+    }
+
+    // vypis do HTML
+
+    for (let j = 0; j < tazenaCisla.length; j++) {
+
+        vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
+
+
+    }
+
 }
 
-// z osudi vyberu 7 nahodnych cisel
-for (let i = 0; i < 7; i++) {
-    let vyherniIndex = Math.floor(Math.random() * osudi.length);
-    let vyherniCislo = osudi[vyherniIndex];
-    
-    tazenaCisla.push(vyherniCislo);
-    osudi.splice(vyherniIndex, 1);
-}
 
-// vypis do HTML
-for (let j = 0; j < tazenaCisla.length; j++) {
-    vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
-}
+
+
+// Udělej losování klasické Sportky
+// Máš osudí čísel od 1 do 48
+// Náhodně z tohoto osudí vyber 7 čísel
+// Žádné číslo se nesmí v tahu opakovat
+// (je vyjmuté z osudí, takže už ho nemůžeš znovu vylosovat)
+// Z každého vylosovaného čísla vygeneruj následující HTML kód:
+// <span class="cislo">8</span>
+// ... který pak přidej dovnitř prvku <div id="vyherni-cisla">:
